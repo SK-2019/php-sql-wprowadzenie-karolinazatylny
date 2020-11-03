14c89e0edbba67e2ab7edeaced91352f381d1086
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-11-03 09:49:32 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -11,8 +11,24 @@
 <?php
 
     require("connect.php");
-    echo("<h2>Zadanie 1 - SELECT * FROM pracownicy where imie not like '%a'</h2>");
-    $result = $conn->query("SELECT * FROM pracownicy where imie not like '%a'");
+    echo("<h2>Zadanie 1: SELECT * FROM pracownicy</h2>");
+    $result = $conn->query('SELECT imie, zarobki, data_urodzenia,nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org');
+        echo("<table border=1>");
+        echo("<th>Imie</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>Data_Urodzenia</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+
+                echo("</tr>");
+            }
+
+        echo("</table>");
+    require("connect.php");
+    echo("<h2>Zadanie 2: SELECT * FROM pracownicy where imie not like %a</h2>");  
+    $result = $conn->query('SELECT * FROM pracownicy where imie not like "%a"'); 
         echo("<table border=1>");
         echo("<th>Id</th>"); 
         echo("<th>Imie</th>");
@@ -26,43 +42,9 @@
             }
 
         echo("</table>");
-        require("connect.php");  
-        echo("<h2>Zadanie 2 - SELECT * FROM pracownicy</h2>");
-    $result = $conn->query("SELECT * FROM pracownicy"); 
-        echo("<table border=1>");
-        echo("<th>Id</th>"); 
-        echo("<th>Imie</th>");
-        echo("<th>Dzial</th>");
-        echo("<th>Zarobki</th>");
-            while($row=$result->fetch_assoc()){ 
-                echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
-
-                echo("</tr>");
-            }
-
-        echo("</table>");
-        require("connect.php");  
-        echo("<h2>Zadanie 3 - SELECT * FROM pracownicy where (dzial=1 or dzial=3)</h2>");
-    $result = $conn->query('SELECT * FROM pracownicy where (dzial=1 or dzial=3)'); 
-        echo("<table border=1>");
-        echo("<th>Id</th>"); 
-        echo("<th>Imie</th>");
-        echo("<th>Dzial</th>");
-        echo("<th>Zarobki</th>");
-            while($row=$result->fetch_assoc()){ 
-                echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
-
-                echo("</tr>");
-            }
-
-        echo("</table>");
-
-        echo("</table>");
-        require("connect.php");  
-        echo("<h2>Zadanie 4 - SELECT * FROM pracownicy where dzial=3</h2>");
-    $result = $conn->query('SELECT * FROM pracownicy where dzial=3');
+    require("connect.php");
+    echo("<h2>Zadanie 3: SELECT * FROM pracownicy where (dzial=2 or dzial=1)</h2>");  
+    $result = $conn->query('SELECT * FROM pracownicy where (dzial=2 or dzial=1)'); 
         echo("<table border=1>");
         echo("<th>Id</th>"); 
         echo("<th>Imie</th>");
@@ -78,8 +60,26 @@
         echo("</table>");
 
         echo("</table>");
-        require("connect.php");  
-        echo("<h2>Zadanie 5 - SELECT * FROM pracownicy where zarobki>=35</h2>");
+    require("connect.php");
+    echo("<h2>Zadanie 4: SELECT * FROM pracownicy where dzial=1</h2>");  
+    $result = $conn->query('SELECT * FROM pracownicy where dzial=1');
+        echo("<table border=1>");
+        echo("<th>Id</th>"); 
+        echo("<th>Imie</th>");
+        echo("<th>Dzial</th>");
+        echo("<th>Zarobki</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
+
+                echo("</tr>");
+            }
+
+        echo("</table>");
+
+        echo("</table>");
+    require("connect.php");
+    echo("<h2>Zadanie 5: SELECT * FROM pracownicy where zarobki>=35</h2>");  
     $result = $conn->query('SELECT * FROM pracownicy where zarobki>=35'); 
         echo("<table border=1>");
         echo("<th>Id</th>"); 
