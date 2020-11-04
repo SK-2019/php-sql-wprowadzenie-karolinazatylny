8c7cc3ae1e8fe376bcd821f332844265f6a0dc39
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-11-04 20:22:12 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
 
 <h1>Karolina Zatylny</h1>
+<h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-karolinazatylny"><b>GITHUB</b></a></h3>
 
 <?php
 
@@ -111,6 +112,61 @@
 
         echo("</table>");
 
+        echo("</table>");
+        require("connect.php");
+        echo("<h2>ZADANIE 7: SELECT sum(zarobki) as suma_zarobki FROM pracownicy, organizacja WHERE (dzial = id_org) and (zarobki>45)</h2>");
+        $result = $conn->query('SELECT sum(zarobki) as sz FROM pracownicy, organizacja WHERE (dzial = id_org) and (zarobki>45)'); 
+            echo("<table border=1>");
+            echo("<th>Suma_Zarobków</th>");
+                while($row=$result->fetch_assoc()){ 
+                    echo("<tr>");
+                        echo("<td>".$row["sz"]."</td>");
+                    echo("</tr>");
+                }
+    
+            echo("</table>");
+
+            echo("</table>");
+        require("connect.php");
+        echo("<h2>ZADANIE 8: SELECT count(imie) as liczba_kobiet FROM pracownicy, organizacja WHERE (dzial = id_org and imie like 'a%')</h2>");
+        $result = $conn->query('SELECT count(imie) as lk FROM pracownicy, organizacja WHERE (dzial = id_org and imie like "a%")'); 
+            echo("<table border=1>");
+            echo("<th>Liczba_Kobiet</th>");
+                while($row=$result->fetch_assoc()){ 
+                    echo("<tr>");
+                        echo("<td>".$row["lk"]."</td>");
+                    echo("</tr>");
+                }
+    
+            echo("</table>");
+
+            echo("</table>");
+        require("connect.php");
+        echo("<h2>ZADANIE 9: SELECT count(imie) as liczba_mężczyzn FROM pracownicy, organizacja WHERE (dzial = id_org and imie not like 'a%')</h2>");
+        $result = $conn->query('SELECT count(imie) as lm FROM pracownicy, organizacja WHERE (dzial = id_org and imie not like "a%")'); 
+            echo("<table border=1>");
+            echo("<th>Liczba_Mężczyzn</th>");
+                while($row=$result->fetch_assoc()){ 
+                    echo("<tr>");
+                        echo("<td>".$row["lm"]."</td>");
+                    echo("</tr>");
+                }
+    
+            echo("</table>");
+
+            echo("</table>");
+        require("connect.php");
+        echo("<h2>ZADANIE 10: SELECT max(zarobki) as max FROM pracownicy, organizacja WHERE (dzial = id_org)</h2>");
+        $result = $conn->query('SELECT max(zarobki) as max FROM pracownicy, organizacja WHERE (dzial = id_org)'); 
+            echo("<table border=1>");
+            echo("<th>Max</th>");
+                while($row=$result->fetch_assoc()){ 
+                    echo("<tr>");
+                        echo("<td>".$row["max"]."</td>");
+                    echo("</tr>");
+                }
+    
+            echo("</table>");
 ?>
     
 </body>
