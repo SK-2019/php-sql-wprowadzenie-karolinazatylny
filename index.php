587b41edda1a4702f36a0507ab0b3fea996197c2
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-11-05 11:47:51 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <h3>
     <a href="https://github.com/SK-2019/php-sql-wprowadzenie-karolinazatylny"><b>GITHUB | </b></a>
     <a href="agregat.php"><b>FUNKCJE AGREGUJĄCE | </b></a>
-    <a href="orgPracownicy.php"><b>ORGANIZACJA I PRACOWNICY</b></a>
+    <a href="orgpracownicy.php"><b>ORGANIZACJA I PRACOWNICY</b></a>
 </h3>
 </div>
 <h1>✿ Karolina Zatylny ✿</h1>
