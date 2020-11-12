1d618a83113ceecf655a8afd662200c51d2f76ca
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-11-12 14:40:45 +0100

    Add files via upload

--- orgpracownicy.php
+++ orgpracownicy.php
@@ -153,8 +153,9 @@
 
             echo("</table>");
         require("connect.php");
-        echo("<h2>ZADANIE 9: SELECT imie, zarobki, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) AND (imie not like 'a%') order by nazwa_dzial asc, zarobki asc</h2>");
-        $result = $conn->query('SELECT imie, zarobki, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) AND (imie not like "a%") order by nazwa_dzial asc, zarobki asc'); 
+        $sql='SELECT imie, zarobki, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) AND (imie not like "a%") order by nazwa_dzial asc, zarobki asc';
+        echo("<h2>ZADANIE 9: $sql</h2>");
+        $result = $conn->query($sql); 
             echo("<table border=1>");
             echo("<th>Imie</th>");
             echo("<th>Zarobki</th>");
@@ -167,8 +168,9 @@
     
             echo("</table>");
             require("connect.php");
-        echo("<h2>ZADANIE 10: SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND dzial=4 order by zarobki desc limit 2</h2>");
-        $result = $conn->query('SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND dzial=4 order by zarobki desc limit 2'); 
+            $sql='SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND dzial=4 order by zarobki desc limit 2';
+        echo("<h2>ZADANIE 10: $sql</h2>");
+        $result = $conn->query($sql); 
             echo("<table border=1>");
             echo("<th>Id</th>");
             echo("<th>Imie</th>");
@@ -184,8 +186,9 @@
     
             echo("</table>");
             require("connect.php");
-        echo("<h2>ZADANIE 11: SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND (dzial=4 or dzial=2) order by zarobki desc limit 3</h2>");
-        $result = $conn->query('SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND (dzial=4 or dzial=2) order by zarobki desc limit 3'); 
+            $sql='SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND (dzial=4 or dzial=2) order by zarobki desc limit 3';
+        echo("<h2>ZADANIE 11: $sql</h2>");
+        $result = $conn->query($sql); 
             echo("<table border=1>");
             echo("<th>Id</th>");
             echo("<th>Imie</th>");
@@ -201,8 +204,9 @@
     
             echo("</table>");
             require("connect.php");
-        echo("<h2>ZADANIE 12: SELECT * FROM pracownicy, organizacja WHERE dzial=id_org order by data_urodzenia asc limit 1</h2>");
-        $result = $conn->query('SELECT * FROM pracownicy, organizacja WHERE dzial=id_org order by data_urodzenia asc limit 1'); 
+            $sql='SELECT * FROM pracownicy, organizacja WHERE dzial=id_org order by data_urodzenia asc limit 1';
+        echo("<h2>ZADANIE 12: $sql</h2>");
+        $result = $conn->query($sql); 
             echo("<table border=1>");
             echo("<th>Id</th>");
             echo("<th>Imie</th>");
