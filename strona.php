493cebf6a1d9f240a65b6ae4c6ba5006f1f89046
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-11-17 10:05:27 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,6 +1,16 @@
+<html>
+        <head>
+        <link rel="stylesheet" href="style.css">
+        </head>
+  
+</html> 
+  
 <?php
-echo ("strona.php");
 
+echo($_POST['firstname']);
 echo($_POST['name']);
+echo($_POST['phone']);
+echo($_POST['city']);
+
 
 ?>
