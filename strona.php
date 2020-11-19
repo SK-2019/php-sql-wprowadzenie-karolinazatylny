1d53882e999413bbe009fbedf4a678d47ff9e019
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-11-19 10:18:22 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -6,11 +6,14 @@
 </html> 
   
 <?php
+include("connect.php");
+echo("<li> imie: ".$_POST['firstname']."</li>");
+echo("<li> nazwisko: ".$_POST['name']."</li>");
+echo("<li> numer tel: ".$_POST['phone']."</li>");
+echo("<li> miasto: ".$_POST['city']."</li>");
 
-echo($_POST['firstname']);
-echo($_POST['name']);
-echo($_POST['phone']);
-echo($_POST['city']);
 
+$sql="INSERT INTO 'pracownicy'('id', 'imie', 'dzial', 'zarobki', 'data_urodzenia') VALUES (null,'józef',2,23,'2001-10-23')";
+$conn->query($sql);
 
 ?>
