f517a82364c10692524403b11b19dec80e6ba8cb
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-11-19 11:28:50 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,6 +1,6 @@
 <html>
         <head>
-        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="style.css">
         </head>
   
 </html> 
@@ -8,9 +8,10 @@
 <?php
 include("connect.php");
 echo("<li> imie: ".$_POST['firstname']."</li>");
-echo("<li> nazwisko: ".$_POST['name']."</li>");
-echo("<li> numer tel: ".$_POST['phone']."</li>");
+echo("<li> nazwisko: ".$_POST['lastname']."</li>");
 echo("<li> miasto: ".$_POST['city']."</li>");
+echo("<li> numer tel: ".$_POST['phone']."</li>");
+echo("<li> kod pocztowy: ".$_POST['poscode']."</li>");
 
 
 $sql="INSERT INTO 'pracownicy'('id', 'imie', 'dzial', 'zarobki', 'data_urodzenia') VALUES (null,'józef',2,23,'2001-10-23')";
