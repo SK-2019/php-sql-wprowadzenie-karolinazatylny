14a33eed956d04a8037e72ff614d98f692e1ef84
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-11-20 14:37:27 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,22 +1,27 @@
 <?php
+echo("jestes w insert.php");
+echo $_POST['name'];
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$servername = "mysql-karolinazatylny.alwaysdata.net";
+$username = "217224";
+$password = "karolcia9";
+$dbname = "karolinazatylny_123";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO Pracownik (null, imie, dzial, zarobki) 
-       VALUES (null,'Balbina', 4, 86,'1999-05-21')";
+$sql = "INSERT INTO Pracownik (null,imie, dzial, zarobki) 
+       VALUES (null,".$_POST['name'].", 1, 76,'1991-11-21')";
+
+echo "<li>".$sql;
+
+
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
-
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
