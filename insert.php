bf8d5a54d86f39313273f36cf7351612e4a852d7
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-11-20 12:05:29 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,38 +1,10 @@
-    <?php
-    echo("jestes w insert.php <br>");
-    echo "<li>". $_POST['name'];
-    echo "<li>". $_POST['dzial'];
-    echo "<li>". $_POST['zarobki'];
-    echo "<li>". $_POST['data_urodzenia'];
-
-
-               $conn = new mysqli("mysql-karolinazatylny.alwaysdata.net","217224","karolcia9","karolinazatylny_123");
-                $result=$conn->query($sql);
-                include("connect.php");
-
-            if ($conn->connect_error) {
-              die("Connection failed: " . $conn->connect_error);
-            }
-            
-            
-            
-    $sql = "INSERT INTO Pracownik (null, name, dzial,zarobki,data_urodzenia)
-            VALUES (
-              null, 
-              $_POST['name'], 
-              $_POST['dzial'], 
-              $_POST['zarobki'],
-              $_POST['data_urodzenia']
-            ";
-
-
-      echo "<li>". $sql;
-
-      if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-      } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-      }
-
-      $conn->close();
-      ?>
+<?php
+        echo("Imie:".$_POST['imie']."<br>");
+        echo("Dzial:".$_POST['dzial']."<br>");
+        echo("Zarobki:".$_POST['zarobki']."<br>");
+        echo("Data:".$POST['data']); 
+      require_once("connect.php");
+      $sql =  "INSERT INTO pracownicy(id_pracownicy, imie, dzial, zarobki, data_urodzenia) VALUES (NULL,'".$_POST['imie']."','".$_POST['dzial']."','".$_POST['zarobki']."','".$POST['data']."')";
+      mysqli_query($conn,$sql);
+      mysqli_close($conn);
+    ?>
