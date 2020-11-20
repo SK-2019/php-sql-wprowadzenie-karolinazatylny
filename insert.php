6187aa033d7da26ce26082e46bf6dd986177d765
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-11-20 12:07:38 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,10 +1,33 @@
-<?php
-        echo("Imie:".$_POST['imie']."<br>");
-        echo("Dzial:".$_POST['dzial']."<br>");
-        echo("Zarobki:".$_POST['zarobki']."<br>");
-        echo("Data:".$POST['data']); 
-      require_once("connect.php");
-      $sql =  "INSERT INTO pracownicy(id_pracownicy, imie, dzial, zarobki, data_urodzenia) VALUES (NULL,'".$_POST['imie']."','".$_POST['dzial']."','".$_POST['zarobki']."','".$POST['data']."')";
-      mysqli_query($conn,$sql);
-      mysqli_close($conn);
-    ?>
+    <?php
+    echo("jestes w insert.php <br>");
+    echo ("<li>". $_POST['name']);
+    echo ("<li>". $_POST['dzial']);
+    echo ("<li>". $_POST['zarobki']);
+    echo ("<li>". $_POST['data_urodzenia']);
+
+
+            
+    $sql = "(INSERT INTO pracownicy (null, name, dzial,zarobki,data_urodzenia)
+            VALUES (
+              null, 
+              $_POST['name'], 
+              $_POST['dzial'], 
+              $_POST['zarobki'],
+              $_POST['data_urodzenia']
+            ))";
+
+ $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+                $result=$conn->query($sql);
+                include("connect.php"); 
+
+
+      echo ($sql);
+
+//       if ($conn->query($sql) === TRUE) {
+//         echo "New record created successfully";
+//       } else {
+//         echo "Error: " . $sql . "<br>" . $conn->error;
+//       }
+
+//       $conn->close();
+      ?>
