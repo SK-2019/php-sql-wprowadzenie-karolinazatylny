413bb623c898d80665ff86f4e1f93b48d7c6a7dd
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-11-20 15:33:44 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -17,10 +17,13 @@ if ($conn->connect_error) {
 }
 
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
-       VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
+VALUES (null,
+					'.$_POST['name'].', 
+					$_POST['dzial'], 
+					$_POST['zarobki'],
+					'.$_POST['data_urodzenia'].');
 
-$conn->query($sql);
-
+echo ("<li>". $sql);
 if ($conn->query($sql) === TRUE) {
   echo ("New record created successfully");
 } else {
