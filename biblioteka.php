94366356b75c1460dae93e3f0e5c632fd41759cc
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-12-01 10:20:15 +0100

    Create biblioteka.php

---
+++ biblioteka.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once("connect.php");
+
+$sql = ("SELECT * from biblTytul");
+echo("<h2>".$sql."</h2>");
+
+$result=$conn->query($sql);
+        echo("<table border=1>");
+        echo("<th>Id</th>");
+        echo("<th>Tytuł</th>");
+
+        while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["id"]."</td><td>".$row["tytul"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
