674a2dbbf43d815504fba07539177d792faf0171
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-12-03 10:55:32 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Karolina Zatylny 2Ti</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+<body>
+<div class="nav"
+<h3>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-karolinazatylny"><b>GITHUB | </b></a>
+    <a href="agregat.php"><b>FUNKCJE AGREGUJĄCE | </b></a>
+    <a href="orgpracownicy.php"><b>ORGANIZACJA I PRACOWNICY | </b></a>
+    <a href="pracownicy.php"><b>PRACOWNICY | </b></a>
+    <a href="dataiczas.php"><b>DATA I CZAS | </b></a> 
+    <a href="formularz.html"><b>FORMULARZ | </b></a>
+    <a href="danedobazy.php">DANE DO BAZY | </b></a>
+    <a href="ksiazki.php">KSIĄŻKI</b></a>
+</h3>
+</div>
+
+<h1>✿ Karolina Zatylny ✿</h1>
+<hr>
+
 <?php
 
 require_once("connect.php");
@@ -6,13 +31,49 @@ $sql = ("SELECT * from biblTytul");
 echo("<h2>".$sql."</h2>");
 
 $result=$conn->query($sql);
-        echo("<table border=1>");
-        echo("<th>Id</th>");
-        echo("<th>Tytuł</th>");
+     echo("<table border=1>");
+     echo("<th>id</th>");
+     echo("<th>tytul</th>");
 
-        while($row=$result->fetch_assoc()) {
+       while($row=$result->fetch_assoc()) {
                 echo("<tr>");
-                    echo("<td>".$row["id"]."</td><td>".$row["tytul"]."</td>");
+                    echo("<td>".$row["tytul_id"]."</td><td>".$row["tytul"]."</td>");
                 echo("</tr>");
             }
         echo("</table>");
+
+
+$sql = ("SELECT * from biblAutor");
+echo("<h2>".$sql."</h2>");
+
+$result=$conn->query($sql);
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>autor</th>");
+
+        while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["autor_id"]."</td><td>".$row["autor"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+
+
+        
+            $sql = ("SELECT * from biblAutor, biblTytul, biblAutor_biblTytul where biblAutor_id=autor_id and biblTytul_id=tytul_id");
+            echo("<h2>".$sql."</h2>");
+            
+            $result=$conn->query($sql);
+                    echo("<table border=1>");
+                    echo("<th>id</th>");
+                    echo("<th>autor</th>");
+                    echo("<th>tytul</th>");
+
+            
+                    while($row=$result->fetch_assoc()) {
+                            echo("<tr>");
+                                echo("<td>".$row["id"]."</td><td>".$row["autor"]."</td><td>".$row["tytul"]."</td>");
+                            echo("</tr>");
+                        }
+                    echo("</table>");
+?>
