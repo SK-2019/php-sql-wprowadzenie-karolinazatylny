f9cb7d5911d5f8657d65911d07fe0fe60eb3e61e
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-12-09 23:12:48 +0100

    Add files via upload

--- ksiazki.php
+++ ksiazki.php
@@ -1,30 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Karolina Zatylny 2Ti</title>
-        <link rel="stylesheet" href="styles.css">
-    </head>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital@1&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Coda+Caption:wght@800&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style.css">
+</head>
 <body>
-<div class="nav"
-<h3>
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-karolinazatylny"><b>GITHUB | </b></a>
-    <a href="index.php"><b>STRONA GŁÓWNA | </b></a>
-    <a href="agregat.php"><b>FUNKCJE AGREGUJĄCE | </b></a>
-    <a href="orgpracownicy.php"><b>ORGANIZACJA I PRACOWNICY | </b></a>
-    <a href="pracownicy.php"><b>PRACOWNICY | </b></a>
-    <a href="dataiczas.php"><b>DATA I CZAS | </b></a> 
-    <a href="formularz.html"><b>FORMULARZ | </b></a>
-    <a href="danedobazy.php">DANE DO BAZY | </b></a>
-    <a href="ksiazki.php">KSIĄŻKI</b></a>
-</h3>
+
+  <h1>K a r o l i n a   ★   Z a t y l n y</h1>
 </div>
 
-<h1>✿ Karolina Zatylny ✿</h1>
-<hr>
+<div class="row">
+  <div class="col-3 col-s-3 menu">
+    <ul>
+    <h6>
+    <li><a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-karolinazatylny"><b>GITHUB</b></a></li>
+    <li><a class="link" href="index.php"><b>STRONA GŁÓWNA</b></a></li>
+    <li><a class="link" href="zadania.php"><b>ZADANIA</b></a></li>
+    <li><a class="link" href="agregat.php"><b>FUNKCJE AGREGUJĄCE</b></a></li>
+    <li><a class="link" href="orgpracownicy.php"><b>ORGANIZACJA I PRACOWNICY</b></a></li>
+    <li><a class="link" href="pracownicy.php"><b>PRACOWNICY</b></a></li>
+    <li><a class="link" href="dataiczas.php"><b>DATA I CZAS</b></a></li>
+    <li><a class="link" href="formularz.html"><b>FORMULARZ</b></a></li>
+    <li><a class="link" href="danedobazy.php"><b>DANE DO BAZY</b></a></li>
+    <li><a class="link" href="ksiazki.php"><b>KSIĄŻKI</b></a></li>
+    </h6>
+    </ul>
+  </div>
 
-<?php
+  <div class="col-6 col-s-9">
+    <h5>Książki</h5>
+ <?php
   
  require_once('connect.php');
 $sql = 'SELECT * FROM biblAutor, biblTytul, biblAutor_biblTytul WHERE biblAutor_id=biblAutor.id and biblTytul_id=biblTytul.id';
@@ -48,33 +56,38 @@ $sql = 'SELECT * FROM biblAutor';
 echo("<h2>".$sql."</h2>");
 
  $result = $conn->query($sql);
-        echo("<table border=1>");
-        echo("<th>Id</th>");
-        echo("<th>Autorzy</th>");
+ echo("<select name='title' id=''title'>");
             while($row=$result->fetch_assoc()){ 
-                echo("<tr>");
-                    echo("<td>".$row["id"]."</td><td>".$row["autor"]."</td>"); 
-
-                echo("</tr>");
+                    echo("<option value=".$row["id"].">".$row["autor"]."</option>"); 
             }
 
-        echo("</table>");
+        echo("</select>");
 
-  
+
 $sql = 'SELECT * FROM biblTytul';
 echo("<h2>".$sql."</h2>");
 
  $result = $conn->query($sql);
-        echo("<table border=1>");
-        echo("<th>Id</th>");
-        echo("<th>Tytuł</th>");
+        echo("<select name='title' id=''title'>");
             while($row=$result->fetch_assoc()){ 
-                echo("<tr>");
-                    echo("<td>".$row["id"]."</td><td>".$row["tytul"]."</td>"); 
-
-                echo("</tr>");
+                echo("<option value=".$row["id"].">".$row["tytul"]."</option>"); 
             }
 
-        echo("</table>");
+        echo("</select>");
 
 ?>
+            <hr>
+              </div>
+            
+              <div class="col-3 col-s-12">
+              <img src="https://i.pinimg.com/originals/21/a7/09/21a709084d7132848f730e245d41c584.jpg">
+            </div>
+            
+              <p>
+              <div class="obrazek">    
+              <img src="https://i.pinimg.com/originals/57/98/5b/57985bf3c5bf34f9c6c41aeb849d3a5b.jpg"></p>
+            </div>
+            </div>
+            
+            </body>
+</html>
