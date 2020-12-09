bf6b9874abc50a8b4e4f93001a5a2549e740681a
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2020-12-09 23:10:09 +0100

    Add files via upload

--- agregat.php
+++ agregat.php
@@ -1,28 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Karolina Zatylny 2Ti</title>
-        <link rel="stylesheet" href="styles.css">
-    </head>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital@1&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Coda+Caption:wght@800&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style.css">
+</head>
 <body>
-<h3>
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-karolinazatylny"><b>GITHUB | </b></a>
-    <a href="index.php"><b>STRONA GŁÓWNA | </b></a>
-    <a href="agregat.php"><b>FUNKCJE AGREGUJĄCE | </b></a>
-    <a href="orgpracownicy.php"><b>ORGANIZACJA I PRACOWNICY | </b></a>
-    <a href="pracownicy.php"><b>PRACOWNICY | </b></a>
-    <a href="dataiczas.php"><b>DATA I CZAS | </b></a> 
-    <a href="formularz.html"><b>FORMULARZ | </b></a>
-    <a href="danedobazy.php">DANE DO BAZY | </b></a>
-    <a href="ksiazki.php">KSIĄŻKI</b></a>
-</h3>
 
-<h1>✿ Karolina Zatylny ✿</h1>
-<hr>
+  <h1>K a r o l i n a   ★   Z a t y l n y</h1>
+</div>
 
-<?php
+<div class="row">
+  <div class="col-3 col-s-3 menu">
+    <ul>
+    <h6>
+    <li><a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-karolinazatylny"><b>GITHUB</b></a></li>
+    <li><a class="link" href="index.php"><b>STRONA GŁÓWNA</b></a></li>
+    <li><a class="link" href="zadania.php"><b>ZADANIA</b></a></li>
+    <li><a class="link" href="agregat.php"><b>FUNKCJE AGREGUJĄCE</b></a></li>
+    <li><a class="link" href="orgpracownicy.php"><b>ORGANIZACJA I PRACOWNICY</b></a></li>
+    <li><a class="link" href="pracownicy.php"><b>PRACOWNICY</b></a></li>
+    <li><a class="link" href="dataiczas.php"><b>DATA I CZAS</b></a></li>
+    <li><a class="link" href="formularz.html"><b>FORMULARZ</b></a></li>
+    <li><a class="link" href="danedobazy.php"><b>DANE DO BAZY</b></a></li>
+    <li><a class="link" href="ksiazki.php"><b>KSIĄŻKI</b></a></li>
+    </h6>
+    </ul>
+  </div>
+
+  <div class="col-6 col-s-9">
+    <h5>Funkcje agregujące</h5>
+ <?php
+
 
     require("connect.php");
     $sql='SELECT sum(zarobki) as sz FROM pracownicy';
@@ -249,7 +260,23 @@
                 echo("</table>");
 
                 
-?>
-    
-</body>
-</html>
+                ?>
+                <hr>
+                  </div>
+                
+                  <div class="col-3 col-s-12">
+                  <img src="https://i.pinimg.com/originals/21/a7/09/21a709084d7132848f730e245d41c584.jpg">
+                  <img src="https://i.pinimg.com/originals/cb/01/ad/cb01add37be89cbf3b7569a8114d1c0e.jpg">
+                  <img src="https://i.pinimg.com/originals/ad/14/dc/ad14dc698af234bfa7a67734ab70a2c7.jpg">
+                  <img src="https://i.pinimg.com/originals/f5/18/13/f5181303b2ff4fdd8d7bebd8cb975283.jpg">
+                  </div>
+                </div>
+                
+                  <p>
+                  <div class="obrazek">    
+                  <img src="https://i.pinimg.com/originals/57/98/5b/57985bf3c5bf34f9c6c41aeb849d3a5b.jpg"></p>
+                </div>
+                </div>
+                
+                </body>
+                </html>
