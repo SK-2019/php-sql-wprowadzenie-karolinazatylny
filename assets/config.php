fa8a7596b1fc54bd3b9d8721d15650fcbae440f4
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2021-02-23 10:20:07 +0100

    Add files via upload

---
+++ assets/config.php
@@ -0,0 +1,6 @@
+<?php
+$servername['servername'] = "mysql-karolinazatylny.alwaysdata.net";
+$password['password'] = "karolcia9";
+$username['username'] = "karolinazatylny_123";
+$dbname['dbname'] = "217224";
+?>
--- assets/connect.php
+++ assets/connect.php
@@ -1,7 +1,4 @@
 <?php
-$conn= new mysqli('mysql-karolinazatylny.alwaysdata.net','217224','karolcia9','karolinazatylny_123');
-if ($conn->connect_error) {
-    die("connection failed: ".mysqli_connect_error());
-
-}
+//require_once("config.php");
+$conn = new mysqli('mysql-karolinazatylny.alwaysdata.net','217224','karolcia9','karolinazatylny_123');
 ?>
