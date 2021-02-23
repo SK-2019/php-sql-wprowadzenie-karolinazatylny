57c4bd63afdbab842bc7ea7ffd34af8041e86718
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2021-02-23 10:22:53 +0100

    Delete config.php

--- assets/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$servername['servername'] = "mysql-karolinazatylny.alwaysdata.net";
-$password['password'] = "karolcia9";
-$username['username'] = "karolinazatylny_123";
-$dbname['dbname'] = "217224";
-?>
