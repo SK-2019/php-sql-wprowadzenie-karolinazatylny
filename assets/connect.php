e12a6c8b977175b859c9c0f4cd6d28c4d103d905
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2021-02-25 12:35:35 +0100

    Add files via upload

--- assets/connect.php
+++ assets/connect.php
@@ -1,5 +1,9 @@
 <?php
-//require_once("config.php");
+
+$hostname = $_SERVER['HTTP_HOST'];
+if ($hostname === '127.0.0.1'){
+    require_once("config.php");}
+
     $conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password'],$_SERVER['dbname']);
     if ($conn->connect_error) {
         die("connection failed: ".mysqli_connect_error());
