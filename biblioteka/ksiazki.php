dfae1c5bddf95d9e3088a65c3a9abf278efc5fea
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2021-04-24 17:38:59 +0200

    Add files via upload

--- biblioteka/ksiazki.php
+++ biblioteka/ksiazki.php
@@ -11,26 +11,10 @@
 </head>
 <body>
 
-  <h1>K a r o l i n a   ★   Z a t y l n y</h1>
-</div>
-
-<div class="row">
-  <div class="col-3 col-s-3 menu">
-    <ul>
-    <h6>
-    <li><a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-karolinazatylny"><b>GITHUB</b></a></li>
-      <li><a class="link" href="/index.php"><b>STRONA GŁÓWNA</b></a></li>
-      <li><a class="link" href="/pracownicy-organizacja/zadania.php"><b>ZADANIA</b></a></li>
-      <li><a class="link" href="/pracownicy-organizacja/agregat.php"><b>FUNKCJE AGREGUJĄCE</b></a></li>
-      <li><a class="link" href="/pracownicy-organizacja/orgpracownicy.php"><b>ORGANIZACJA I PRACOWNICY</b></a></li>
-      <li><a class="link" href="/pracownicy-organizacja/pracownicy.php"><b>PRACOWNICY</b></a></li>
-      <li><a class="link" href="/pracownicy-organizacja/dataiczas.php"><b>DATA I CZAS</b></a></li>
-      <li><a class="link" href="/dane-do-bazy/formularz.html"><b>FORMULARZ</b></a></li>
-      <li><a class="link" href="/dane-do-bazy/danedobazy.php"><b>DANE DO BAZY</b></a></li>
-      <li><a class="link" href="/biblioteka/ksiazki.php"><b>KSIĄŻKI</b></a></li>
-    </h6>
-    </ul>
-  </div>
+<?php
+      include("../assets/header.php");
+      include("../assets/menu.php");
+?>
 
   <div class="col-6 col-s-9">
     <h5>Książki</h5>
