e93ca77cdc5ea9a0b758a545845e134dd5a26fef
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2021-04-24 19:38:59 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 <?php
       include("assets/header.php");
-      include("../assets/menu.php");
+      include("assets/menu.php");
 ?>
   <div class="col-6 col-s-9">
     <h5>Strona Główna</h5>
