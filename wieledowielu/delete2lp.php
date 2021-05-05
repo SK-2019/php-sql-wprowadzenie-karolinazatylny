b2bae785f02819390f3ec6420392dfdd7e63e77b
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2021-05-05 19:36:01 +0200

    Add files via upload

--- wieledowielu/delete2lp.php
+++ wieledowielu/delete2lp.php
@@ -1,6 +1,6 @@
 <?php
 require("../assets/connect.php");
-$sql = "DELETE FROM pacjenet where id_pacjent='".$_POST['id']."'";
+$sql = "DELETE FROM pacjent where id_pacjent='".$_POST['id']."'";
 $conn->query($sql);
 header("location:lekarzpacjent.php");
 ?>
