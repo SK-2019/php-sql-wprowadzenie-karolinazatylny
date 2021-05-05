a7ff72d67479f1a20202b55efb84c5f45e28be93
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2021-05-05 18:18:03 +0200

    Add files via upload

--- wieledowielu/delete1kn.php
+++ wieledowielu/delete1kn.php
@@ -1,6 +1,6 @@
 <?php
 require("../assets/connect.php");
-$sql = "DELETE FROM klasa_nauczyciel where id='".$_POST['id']."'";
+$sql = "DELETE FROM nauczyciel_klasa where id='".$_POST['id']."'";
 $conn->query($sql);
 header("location:nauczycielklasa.php");
 ?>
