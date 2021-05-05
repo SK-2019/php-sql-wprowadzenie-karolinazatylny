5fc6f074cabf06f5eb6f244c8a0bd31b66e67b48
Karolina Zatylny <63092911+karolinazatylny@users.noreply.github.com>
2021-05-05 18:13:36 +0200

    Add files via upload

--- wieledowielu/delete3kn.php
+++ wieledowielu/delete3kn.php
@@ -1,25 +1,6 @@
 <?php
-  require_once("../assets/connect.php");
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-//definiujemy zapytanie $sql
-$sql = "DELETE  FROM nauczyciel WHERE id_nauczyciel= '".$_POST['id']."';";
-
-//wyświetlamy zapytanie $sql
-echo $sql;
-
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-
-$conn->close();
-
+require("../assets/connect.php");
+$sql = "DELETE FROM nauczyciel where id_nauczyciel='".$_POST['id']."'";
+$conn->query($sql);
 header("location:nauczycielklasa.php");
 ?>
